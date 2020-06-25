4f68943b7ee4e15ee7875ed5c03ae879ed7287fc
Khusharth <khusharth19@gmail.com>
2020-06-26 01:54:02 +0530

    Configured React Router for the Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
 import GlobalStyle from "../styles/GlobalStyle";
+import history from "../history";
+import Home from "../pages/Home";
+import Likes from "../pages/Likes";
 
 const App = () => {
     return (
         <>
             <GlobalStyle />
-            <div>App</div>
+            <Router history={history}>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/likes" exact component={Likes} />
+                </Switch>
+            </Router>
         </>
     );
 };
