5ccfd64ffdcd3138865c7917ce0a5f6721e4f7a3
Khusharth <khusharth19@gmail.com>
2020-06-28 11:34:13 +0530

    Completed Home Page

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import App from "./components/App";
-import reducers from "./reducers";
+import reducers, { saveToLocalStorage, loadFromLocalStorage } from "./reducers";
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const persistedStore = loadFromLocalStorage();
+const store = createStore(reducers, persistedStore, applyMiddleware(thunk));
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 ReactDOM.render(
     <Provider store={store}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { connect } from 'react-redux';
+import { fetchStoryIDs, fetchStories } from "../actions";
+import NewsList from "../components/NewsList";
+import Loader from "../components/Loader";
+import { hasMoreStoriesSelector } from '../reducers/selector';
 
-const Home = () => {
+
+const Home = ({ fetchStoryIDs, fetchStories, storyIds, page, isFetching, stories, hasMoreStories, isLiked }) => {
+
+    const fetchStory = () => {
+        console.log('called')
+        if (!isFetching) {
+            console.log(storyIds);
+            fetchStories({ storyIds, page });
+        }
+    }
+
+    useEffect(() => {
+        if (stories.length === 0) {
+            fetchStoryIDs();
+        }
+    }, []);
+
+    console.log(stories.length)
+
     return (
-        <>
-            <div>Home</div>
-        </>
+        <main>
+            <InfiniteScroll
+                dataLength={stories.length}
+                next={fetchStory}
+                hasMore={hasMoreStories}
+                loader={<Loader />}
+                style={{
+                    height: '100%',
+                    overflow: 'visible'
+                }}
+            >
+                <NewsList stories={stories} />
+            </InfiniteScroll>
+        </main>
     );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+        stories: state.story.stories,
+        page: state.story.page,
+        storyIds: state.story.storyIds,
+        isFetching: state.story.isFetching,
+        hasMoreStories: hasMoreStoriesSelector(state),
+    }
+};
+
+
+
+export default connect(mapStateToProps, { fetchStoryIDs, fetchStories })(Home);
